329fdf45ba34d5443315955a94100d92329aaf55
Will Hogan <willhogan11@hotmail.com>
2016-06-21 13:54:48 +0100

    Working on holding Multiple grades

--- src/TestClass.java
+++ src/TestClass.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -7,18 +10,29 @@ public class TestClass {
 
 		String module = "";
 		double grade, percentage;		
-		
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Enter your module title: ");
-		module = scan.nextLine();
-		System.out.println("Enter your Grade: ");
-		grade = scan.nextDouble();
-		System.out.println("Enter your percentage of the module");
-		percentage = scan.nextDouble();
+		List<Double> resultStore = new ArrayList<Double>();
 		
-		calcGrade(module, grade, percentage);
+		do{
+			if(module.equals("exit")){
+				System.out.println("Exiting...");
+				System.exit(0);
+			}else{
+				System.out.println("Enter your module title: ");
+				module = scan.next();
+				System.out.println("Enter your Grade: ");
+				grade = scan.nextDouble();
+				System.out.println("Enter your percentage of the module");
+				percentage = scan.nextDouble();
+				resultStore.add(calcGrade(module, grade, percentage));
+			}
+		}while(!module.equals("exit"));
 		
 		scan.close();
+		
+		for (Double d : resultStore) {
+			System.out.println(d);
+		}
 	}
 	
 	public static double calcGrade(String module, double grade, double percentage){
@@ -26,4 +40,12 @@ public class TestClass {
 		System.out.println("Result for module " + module + " is " + result + "% / " + percentage + "%");
 		return result;
 	}
+	
+/*	public static void storeResult(double result){
+		List<Double> resultStore = new ArrayList<Double>();
+		resultStore.add(result);
+		for (Double d : resultStore) {
+			System.out.println(d);
+		}
+	}*/
 }
