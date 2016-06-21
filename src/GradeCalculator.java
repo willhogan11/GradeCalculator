eb2a8dd295c386c7001c0a3239f02d1e594676a7
Will Hogan <willhogan11@hotmail.com>
2016-06-21 22:09:59 +0100

    Seperated code out into functions

--- src/GradeCalculator.java
+++ src/GradeCalculator.java
@@ -3,15 +3,35 @@ import java.util.List;
 import java.util.Scanner;
 
 
-public class TestClass {
+public class GradeCalculator {
 
 	public static void main(String[] args) {
-
+		gatherDataMenu();
+	}
+	
+	public static void displayResults(List<Double> resultStore){
+		double sum = 0;
+		for (Double d : resultStore) {
+			System.out.println("Score : " + d);
+			sum = sum + d;
+		}
+		System.out.println("Total for module : " + sum + "%");
+	}
+	
+	
+	public static double calcGrade(String module, double grade, double percentage){
+		double result = grade * percentage / 100;
+		System.out.println("Result for module " + module + " is " + result + "% / " + percentage + "%");
+		return result;
+	}
+	
+	
+	public static int gatherDataMenu(){
 		int choice;
 		String module;		
-		double grade, percentage, sum = 0;		
-		Scanner scan = new Scanner(System.in);
+		double grade, percentage;		
 		List<Double> resultStore = new ArrayList<Double>();
+		Scanner scan = new Scanner(System.in);
 		
 		do{
 			System.out.println("\nEnter 1 to Enter data, 2 to Exit");
@@ -39,28 +59,10 @@ public class TestClass {
 				}
 			}
 		}while(choice < 2);
-		
-		// For visibility display
-		for (Double d : resultStore) {
-			System.out.println("Score : " + d);
-			sum = sum + d;
-		}
-		System.out.println("Total for module : " + sum + "%");
-		
 		scan.close();
+		
+		displayResults(resultStore);
+		
+		return choice;
 	}
-	
-	public static double calcGrade(String module, double grade, double percentage){
-		double result = grade * percentage / 100;
-		System.out.println("Result for module " + module + " is " + result + "% / " + percentage + "%");
-		return result;
-	}
-	
-/*	public static void storeResult(double result){
-		List<Double> resultStore = new ArrayList<Double>();
-		resultStore.add(result);
-		for (Double d : resultStore) {
-			System.out.println(d);
-		}
-	}*/
 }
---
+++ src/GradeImpl.java
@@ -0,0 +1,27 @@
+
+public class GradeImpl implements Gradable{
+
+	public double calculateGrade(String module, double grade, double percentage) {
+		return 0;
+	}
+	
+	public void options() {
+		
+	}
+
+	public void result() {
+		
+	}
+
+	public void percentage() {
+		
+	}
+
+	public void grade() {
+		
+	}
+
+	public void listResult() {
+		
+	}
+}
